24909a71bbcb569379b81980b9e8959e22819c2d
Yuya8846 <zm20208@josai.ac.jp>
2021-11-02 13:25:10 +0000

    2021/11/02

--- ex6-1.js
+++ ex6-1.js
@@ -1,9 +1,2 @@
 function sayhello61(){
-    alert('Hello,Function');
-}
-function saygoodbye61(){
-    alert('Goodbye');
-}
-function sayhello67(){
-    alert('Hello');
-}
+    alert('Hello,Function');}
