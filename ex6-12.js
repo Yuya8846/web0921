10221f2caa1816f2bc7fcbba0fb445e8a20b810f
Yuya8846 <zm20208@josai.ac.jp>
2022-02-13 12:35:40 +0000

    2022/02/13

--- ex6-12.js
+++ ex6-12.js
@@ -1,11 +1,15 @@
-function hyoji2(){
-    //表示する文字varstr= "一文字ずつ表示します。"; 
+function hyoji1(){
+    //表示する文字
+    var str= "一文字ずつ表示します。";
     //テキストボックスの文字数
-    varcnt= document.timer2.moji2.value.length; 
-    //文字が全部表示されているか確認if ( cnt< 11 ) { 
-        //現在より1文字多く切り出して表示
-        document.timer2.moji2.value= str.substr(0,cnt+1); } else { 
-            //全て表示されたら、空文字に戻す
-            document.timer2.moji2.value= ""; } 
-            //setTimeout()を含む関数を呼び出す
-            setTimeout2("hyoji2()",1000); }
+    var cnt=document.timer1.moji1.value.length;
+    //文字が全部表示されているか確認
+    if(cnt<11){
+    //現在より1文字多く切り出して表示
+    document.timer1.moji1.value=str.substr(0,cnt+1);}
+    //全て表示されたら、空文字に戻す
+    else{
+    document.timer1.moji1.value="";}
+    //setTimeout()を含む関数を呼び出す
+    setTimeout("hyoji1()",1000);
+}
